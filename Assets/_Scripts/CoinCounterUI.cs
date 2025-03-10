30affb5551984b345dd6f87d209c9e1f70e6373c
lucywalt <lucywltrs@gmail.com>
2025-03-10 15:51:07 -0700

    Created sliding score animation

---
+++ Assets/_Scripts/CoinCounterUI.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using TMPro;
+
+public class CoinCounterUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private TextMeshProUGUI current;
+    [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private Transform coinTextContainer;
+    [SerializeField] private float duration;
+
+    private float containerInitPosition;
+    private float moveAmount;
+
+
+    public void UpdateScore(int score)
+    {
+        toUpdate.SetText($"{score}"); //set score to masked text UI
+        //trigger the local move animation
+        coinTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration);
+
+        //start a coroutine
+        StartCoroutine(ResetCoinContainer(score));
+
+    }
+
+    private IEnumerator ResetCoinContainer(int score)
+    {
+        //tells editor to wait for a given period of time
+        yield return new WaitForSeconds(duration);
+
+        //we use duration since thats the same as the animation
+        current.SetText($"{score}"); //update original score
+        Vector3 localPosition = coinTextContainer.localPosition;
+        coinTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
+        //then rest the y local position of the coin text container
+
+    }
+
+    void Start()
+    {
+        Canvas.ForceUpdateCanvases();
+        current.SetText("0");
+        toUpdate.SetText("0");
+        containerInitPosition = coinTextContainer.localPosition.y;
+        moveAmount = current.rectTransform.rect.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
